d66e6912af2e41af15acc32c152ecea43e99dbc6
Prince Kumar <princek63055@gmail.com>
2024-01-20 17:12:30 +0530

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = File::get(path:'database/json/users.json');
+
+        $users = collect(json_decode($json));
+    
+        $users->each(function($user){
+            User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'password' => bcrypt($user->password),
+                'role' => $user->role
+            ]);
+        });
+    }
+}
